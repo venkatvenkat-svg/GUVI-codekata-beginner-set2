6dc95b6a51d6b7daf53d63f6241ad02ca773140c
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-07-22 19:03:11 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -2,14 +2,11 @@ num=input()
 num=int(num)
 rev=0
 temp=num
-if(num<=1000):
-    while(num!=0):
-        rem=num%10
-        rev=(rev*10)+rem
-        num=num//10
-    if(temp==rev):
-        print("yes")
-    else:
-        print("no")
+while(num!=0):
+    rem=num%10
+    rev=(rev*10)+rem
+    num=num//10
+if(temp==rev):
+    print("yes")
 else:
-    print("invalid number")
+    print("no")
