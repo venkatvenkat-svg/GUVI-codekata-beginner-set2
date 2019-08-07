7ba9c4ac79f524c87dc298107c462531cbd76e1d
venkatvenkat-svg <53077792+venkatvenkat-svg@users.noreply.github.com>
2019-08-07 18:40:40 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
 n=int(input())
-fact=1
-if(n>=1):
+if(n==0):
+    fact=1
+else:
+    fact=1
     for i in range(1,n+1):
         fact=fact*i
-    print(fact)
-else:
-    print("enter positive integer")
+print(fact)
